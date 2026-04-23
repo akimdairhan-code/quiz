40486db70493f8972d319323778afaa1bd428034
akimdairhan-code <akimdairhan@gmail.com>
2026-04-24 02:21:08 +0500

    Update layout.tsx
    git push

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -19,7 +19,31 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <header>
+<<<<<<< Updated upstream
           <NavBar />
+=======
+          <nav className={styles.nav} style={{ paddingLeft: "32px" }}>
+            <div className={styles.navLinks}>
+              {navLinks.map(({ href, label }) => (
+                <Link key={href} href={href}>
+                  {label}
+                </Link>
+              ))}
+            </div>
+
+            {isLoggedIn ? (
+              <form action="/auth/logout" method="post">
+                <button type="submit">Logout</button>
+              </form>
+            ) : (
+              <form action="/auth/login" method="post">
+                <input type="text" placeholder="Email" name="email" />
+                <input type="password" placeholder="Password" name="password" />
+                <button type="submit">Login</button>
+              </form>
+            )}
+          </nav>
+>>>>>>> Stashed changes
         </header>
         <main className={styles.main}>{children}</main>
       </body>
